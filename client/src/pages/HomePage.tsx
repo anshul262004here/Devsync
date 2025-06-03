991487a91a18da8821b1e9b8119c724cd3007772
anshul262004here <anshuljangale000@gmail.com>
2025-06-04 01:38:33 +0530

    Initial commit

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import illustration from "@/assets/illustration.svg"
+import ilu from "@/assets/ilu.gif"
 import FormComponent from "@/components/forms/FormComponent"
 // import Footer from "@/components/common/Footer";
 
@@ -8,7 +8,7 @@ function HomePage() {
             <div className="my-12 flex h-full min-w-full flex-col items-center justify-evenly sm:flex-row sm:pt-0">
                 <div className="flex w-full animate-up-down justify-center sm:w-1/2 sm:pl-4">
                     <img
-                        src={illustration}
+                        src={ilu}
                         alt="Code Sync Illustration"
                         className="mx-auto w-[250px] sm:w-[400px]"
                     />
